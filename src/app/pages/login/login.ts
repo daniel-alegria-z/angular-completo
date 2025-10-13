ee9c8befd045bc9c5a945cdb76a3138aac67c14c
daniel-alegria-z <danialegria00@gmail.com>
2025-10-13 17:42:54 -0500

    Subida del login con todos sus estilos e imagenes correspondientes

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+export class Login {
+  username = '';
+  password = '';
+
+  onSubmit() {
+    console.log('Usuario:', this.username, 'Contraseña:', this.password);
+  }
+}
