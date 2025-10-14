8769ca5832f0d18ae10b285c1c15772a9675813e
daniel-alegria-z <danialegria00@gmail.com>
2025-10-14 18:28:48 -0500

    2

--- src/app/pages/inicio/inicio.ts
+++ src/app/pages/inicio/inicio.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css'
--- src/app/pages/servicios/servicios.ts
+++ src/app/pages/servicios/servicios.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-servicios',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './servicios.html',
   styleUrl: './servicios.css'
