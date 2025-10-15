1a8ef224adf839dbc8d8a89f308cc6e2f43e0a59
daniel-alegria-z <danialegria00@gmail.com>
2025-10-14 23:53:29 -0500

    subida de la interfaz total

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { Inicio } from './pages/inicio/inicio';
 import { Servicios } from './pages/servicios/servicios';
 
 export const routes: Routes = [
-  { path: '', component: Login },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
   { path: 'inicio', component: Inicio },
   { path: 'servicios', component: Servicios }
 ];
